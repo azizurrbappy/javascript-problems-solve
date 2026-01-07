d90b326a490d42e98811f5be807d344239d2eb85
azizurrbappy <azizurr.bappy@gmail.com>
2026-01-08 01:26:40 +0600

    Problem 2: Vowel Counting

---
+++ problem-2:vowel-counting.js
@@ -0,0 +1,23 @@
+/*
+Problem 2: Count Vowels in a String
+Write a function that counts how many vowels (a, e, i, o, u) are in a given string.
+
+Example:
+
+Input: "programming"
+Output: 3
+*/
+
+function countVowels(str) {
+  let count = 0;
+  const vowels = 'aeiouAEIOU';
+
+  for (let char of str) {
+    if (vowels.includes(char)) {
+      count++;
+    }
+  }
+  return count;
+}
+
+console.log(countVowels('programming'));
