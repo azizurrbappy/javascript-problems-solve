471ef12e66c9af98d6160c7d238cbdbeb8732427
azizurrbappy <azizurr.bappy@gmail.com>
2026-01-08 00:39:47 +0600

    Problem 7: Find Even Numbers in an Array

---
+++ problem-7:finding-even-numbers.js
@@ -0,0 +1,15 @@
+/*
+Problem 7: Find Even Numbers in an Array
+Write a function that returns all even numbers from a given array.
+
+Example:
+
+Input: [1, 2, 3, 4, 5, 6]
+Output: [2, 4, 6]
+*/
+
+function getEvenNumbers(array) {
+  return arr.filter(n => n % 2 === 0);
+}
+
+console.log(getEvenNumbers([1, 2, 3, 4]));
