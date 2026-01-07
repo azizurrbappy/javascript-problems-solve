d0758ef9e3e37fddc4c02e7f8f46fa427af16cfd
azizurrbappy <azizurr.bappy@gmail.com>
2026-01-08 01:34:33 +0600

    Problem 4: Largest number in an array

---
+++ problem-4:largest-number-in-an-array.js
@@ -0,0 +1,19 @@
+/*
+Problem 4: Find the Maximum Number
+Write a function that takes an array of numbers and returns the largest number.
+
+Example:
+
+Input: [5, 1, 9, 3]
+Output: 9
+*/
+
+function findMax(arr) {
+  let max = arr[0];
+  for (let num of arr) {
+    if (num > max) max = num;
+  }
+  return max;
+}
+
+console.log(findMax([5, 1, 9, 3]));
