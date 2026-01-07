9e7af6a812ce1ca27f43f841283059f7aa6cf909
azizurrbappy <azizurr.bappy@gmail.com>
2026-01-08 01:15:21 +0600

    Problem 8: Capitalize the first letter of each word

---
+++ problem-8:capitalize-the-first-letter-of-each-word.js
@@ -0,0 +1,18 @@
+/* 
+Problem 8: Capitalize First Letter of Each Word
+Write a function that capitalizes the first letter of each word in a string.
+
+Example:
+
+Input: "hello world"
+Output: "Hello World"
+*/
+
+function capitalizeWords(str) {
+  return str
+    .split(' ')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
+}
+
+console.log(capitalizeWords('hello world'));
